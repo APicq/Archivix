a847be1687c86ea17a98e9f7fb19698549e7607d
APicq <ant.pic.biz@gmail.com>
2014-01-28 14:40:09 +0100

    Minor fixes

--- src/main/java/apicq/archivix/tools/ReportWorker.java
+++ src/main/java/apicq/archivix/tools/ReportWorker.java
@@ -1,14 +1,20 @@
 package apicq.archivix.tools;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 import java.util.List;
 import java.util.logging.Logger;
+
+
+
+// TODO add scrollable
 
 /**
  * Created by pic on 1/27/14.
  * A specialized worker. Prints a dialog with informations.Needs to be subclassed.
  *
-
  */
 public class ReportWorker extends SwingWorker<Boolean,String> {
 
@@ -20,26 +26,38 @@ public class ReportWorker extends SwingWorker<Boolean,String> {
 
     private class ReportWorkerDialog extends JDialog {
 
-        public ReportWorkerDialog(JFrame myFrame){
+        public ReportWorkerDialog(JFrame myFrame, String name){
 
             super(myFrame);
+            setTitle(name);
 
 
             JTextArea textArea = new JTextArea();
             textArea.setEditable(false);
             textArea.setText("Wait..");
+            textArea.setColumns(40);
+            textArea.setRows(30);
 
             JButton OKButton = new JButton("OK");
             JButton CancelButton = new JButton("Cancel");
 
             JPanel vertPanel = new JPanel();
-            vertPanel.setLayout(new BoxLayout(vertPanel,BoxLayout.PAGE_AXIS));
-            vertPanel.add(textArea);
+            vertPanel.setBorder(new EmptyBorder(10,10,10,10));
+            vertPanel.setLayout(new BoxLayout(vertPanel, BoxLayout.PAGE_AXIS));
+            //vertPanel.add(textArea);
+
+            JScrollPane scrollPane = new JScrollPane(textArea);
+            vertPanel.add(scrollPane);
 
             JPanel panel = new JPanel();
-            panel.setLayout(new BoxLayout(panel,BoxLayout.LINE_AXIS));
+            BoxLayout layout = new BoxLayout(panel,BoxLayout.LINE_AXIS);
+            //panel.setLayout(layout);
             panel.add(OKButton);
+
             panel.add(CancelButton);
+            panel.add(Box.createHorizontalGlue());
+
+            panel.setMaximumSize(panel.getPreferredSize());
 
             vertPanel.add(panel);
 
@@ -62,8 +80,8 @@ public class ReportWorker extends SwingWorker<Boolean,String> {
     /**
      * Constructor
      */
-    public ReportWorker(JFrame myFrame){
-        reportWorkerDialog = new ReportWorkerDialog(myFrame);
+    public ReportWorker(JFrame myFrame,String name){
+        reportWorkerDialog = new ReportWorkerDialog(myFrame,name);
 
     }
 
@@ -96,7 +114,7 @@ public class ReportWorker extends SwingWorker<Boolean,String> {
     public static void main(String[] args){
 
         JFrame frame = new JFrame("Test ReportWorkerDialog");
-        ReportWorker worker = new ReportWorker(frame);
+        ReportWorker worker = new ReportWorker(frame,"Worker Dialog");
         worker.doTheJob();
 
 
